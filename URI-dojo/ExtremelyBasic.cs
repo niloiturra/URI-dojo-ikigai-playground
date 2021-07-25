08ac9c357442eda0102847af817170828f65e52a
Nilo Iturra <nilo.alan@hotmail.com>
2021-07-24 21:41:05 -0300

    Trial - URI with xUnit TDD practice

---
+++ URI-dojo/ExtremelyBasic.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace URI_dojo
+{
+    //URI 1001
+    public class ExtremelyBasic
+    {
+        private int Sum(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        
+        [Theory]
+        [InlineData(10, 9, 19)]
+        [InlineData(-10, 4, -6)]
+        [InlineData(15, -7, 8)]
+        public void Should_sum_numbers(int firstNum, int secondNum, int expected)
+        {
+            //arrange & act
+            var result = Sum(firstNum, secondNum);
+
+            //assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ URI-dojo/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace URI_dojo
+{
+    //URI 1000
+    public class HelloWorld
+    {
+        private string Print()
+        {
+            return "Hello World!";
+        }
+        
+        [Fact]
+        public void Should_print_hello_world_in_screen()
+        {
+            //act
+            var result = Print();
+
+            //assert
+            result.Should().Be("Hello World!");
+        }
+    }
+}
